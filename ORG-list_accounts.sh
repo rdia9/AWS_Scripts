835167c9ecb39096f57afe35c871b97945a570f3
Raphaël Diacamille <raphael.diacamille@btp-consultants.fr>
2024-08-22 11:42:14 +0200

    fix headers

--- ORG-list_accounts.sh
+++ ORG-list_accounts.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-echo "Id","Name","Status","JoinedTimestamp" > "$(date +%Y%m%d)_SSO-ReportUserList.csv"
-aws organizations list-accounts --profile RespInfo | jq -r '.Accounts[] | [.Id, .Name, .Status, .JoinedTimestamp] |@csv' > "$(date +%Y%m%d)_AWSOrg_all_accounts.csv"
+echo "Id","Name","Status","JoinedTimestamp" > "$(date +%Y%m%d)_AWSOrg_all_accounts.csv"
+aws organizations list-accounts | jq -r '.Accounts[] | [.Id, .Name, .Status, .JoinedTimestamp] |@csv' >> "$(date +%Y%m%d)_AWSOrg_all_accounts.csv"
